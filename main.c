30fe052f0156f87989bfb75943f0307de595c147
Levin Baenninger <levin.baenninger@buhlergroup.com>
2023-11-09 07:57:00 +0100

    add newEntry function

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Entry
+{
+    char firstName[40];
+    char lastName[50];
+    char number[17];
+    char information[200];
+} entry;
+
 int menu();
+void newEntry(entry *userEntry);
 
 int main()
 {
@@ -13,6 +22,7 @@ int main()
 int menu()
 {
     int choice;
+    entry userEntry;
 
     printf("\n\n+----------------------------+\n");
     printf("| -1- Alle Einträge anzeigen |\n");
@@ -42,11 +52,11 @@ int menu()
     switch (choice)
     {
     case 1:
-        // Erste Funktion verlinken
+        
         break;
 
     case 2:
-        // Zweite Funktion verlinken
+        newEntry(&userEntry);
         break;
 
     case 3:
@@ -62,8 +72,34 @@ int menu()
         break;
 
     case 6:
-        printf("Programm schliessen ...\n");
+        printf("\nProgramm schliessen ...\n");
         return EXIT_SUCCESS;
         break;
     }
+}
+
+void newEntry(entry *userEntry)
+{
+    printf("\n\nNeuer Eintrag (Fuer Umlaute: ae, ue, oe!)");
+    printf("\n----------------------------------------------------------\n\n");
+
+    printf("Was ist der Vorname von deinem Kontakt? ");
+    fflush(stdin);
+    scanf("%[^\n]s", &userEntry->firstName);
+    fflush(stdin);
+
+    printf("Was ist der Nachname von deinem Kontakt? ");
+    scanf("%[^\n]s", &userEntry->lastName);
+    fflush(stdin);
+
+    printf("Was lautet die Telefonnummer von deinem Kontakt? ");
+    scanf("%[^\n]s", &userEntry->number);
+    fflush(stdin);
+
+    printf("Sonstige Informationen? ");
+    scanf("%[^\n]s", &userEntry->information);
+    fflush(stdin);
+
+    FILE *fptr = fopen("contacts.csv", "a");
+    fprintf(fptr, "%s;%s;%s;%s\n", userEntry->firstName, userEntry->lastName, userEntry->number, userEntry->information);
 }
